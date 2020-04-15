86525f9db2aeec7190c27c1d4f7f4f6869de3884
Andrew McCauley <>
2020-04-14 21:44:21 -0500

    Cleanup

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -26,28 +26,25 @@ public class PlayerMovement : MonoBehaviour
         {
             transform.position = new Vector3(transform.position.x, transform.position.y + 1, 0);
             FacingDirection = FacingDirection.North;
-            transform.localEulerAngles = new Vector3(0, 0, 180);// rotation = new Quaternion(0, 0, 180, 0);
+            transform.localEulerAngles = new Vector3(0, 0, 180);
         }
         else if (Input.GetKeyDown(KeyCode.S))
         {
             transform.position = new Vector3(transform.position.x, transform.position.y - 1, 0);
             FacingDirection = FacingDirection.South;
             transform.localEulerAngles = new Vector3(0, 0, 0);
-            //transform.rotation = new Quaternion(0, 0, 0, 0);
         }
         else if (Input.GetKeyDown(KeyCode.A))
         {
             transform.position = new Vector3(transform.position.x - 1, transform.position.y, 0);
             FacingDirection = FacingDirection.West;
             transform.localEulerAngles = new Vector3(0, 0, 270);
-            //transform.rotation = new Quaternion(0, 0, 270, 0);
         }
         else if (Input.GetKeyDown(KeyCode.D))
         {
             transform.position = new Vector3(transform.position.x + 1, transform.position.y, 0);
             FacingDirection = FacingDirection.East;
             transform.localEulerAngles = new Vector3(0, 0, 90);
-            //transform.rotation = new Quaternion(0, 0, 90, 0);
         }
     }
 }
