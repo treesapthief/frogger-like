52aba23fe410712cfb1af2dff499433bc2417c4f
mccaua79 <drewmccauley90@gmail.com>
2020-04-18 21:05:19 -0500

    Moving the cars, logs, and turtles

---
+++ Assets/Scripts/MovingObstacle.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class MovingObstacle : MonoBehaviour
+{
+    public float Speed;
+
+    private Rigidbody2D _rigidbody2D;
+
+    void Start()
+    {
+        _rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _rigidbody2D.velocity = Vector3.right * Speed;
+    }
+}
