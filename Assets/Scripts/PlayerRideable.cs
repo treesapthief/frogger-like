aa68550c78dea3bdb0d5aa9ed36be3a870657dc3
mccaua79 <drewmccauley90@gmail.com>
2020-04-26 08:46:22 -0500

    Player hops onto rideable object

--- Assets/Scripts/PlayerRideable.cs
+++ Assets/Scripts/PlayerRideable.cs
@@ -1,6 +1,26 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
+public static class UtilityExtensions
+{
+    public static T[] GetComponentsOnlyInChildren<T>(this Component script) where T : class
+    {
+        List<T> group = new List<T>();
+
+        //collect only if its an interface or a Component
+        if (typeof(T).IsInterface
+            || typeof(T).IsSubclassOf(typeof(Component))
+            || typeof(T) == typeof(Component))
+        {
+            foreach (Transform child in script.transform)
+            {
+                group.AddRange(child.GetComponentsInChildren<T>());
+            }
+        }
+
+        return group.ToArray();
+    }
+}
 
 public class PlayerRideable : MonoBehaviour
 {
@@ -13,26 +33,42 @@ public class PlayerRideable : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
     }
 
-    void OnTriggerStay2D(Collider2D collision)
+    void OnTriggerEnter2D(Collider2D platform)
     {
-        if (collision.gameObject.tag == "Player")
+        if (platform.gameObject.tag == "Rideable" && gameObject.transform.parent == null)
         {
             Debug.Log("Player landed on ride-able object");
-            collision.gameObject.transform.parent = gameObject.transform;
-            // TODO: attach player as child
+            var playerRigidBody = GetComponent<Rigidbody2D>();
+            var parentRigidBody = platform.gameObject.GetComponent<Rigidbody2D>();
+            var playerPosition = gameObject.transform.position;
+            var children = platform.GetComponentsOnlyInChildren<Transform>().ToList();
+            if (children.Any())
+            {
+                Debug.Log("Choose a side");
+                var newDistance = children.Select(x => x.position).OrderBy(c => Vector3.Distance(c, playerPosition)).FirstOrDefault();
+                Debug.Log($"Player: {playerPosition}, New Distance {newDistance}");
+                gameObject.transform.position = new Vector3(newDistance.x, playerPosition.y, 0);
+            }
+
+            // TODO: Set player to correct "tile" on the turtle
+            // Check player's position to the turtle's position (and length)
+            // Determine if they are on the left or the right
+            playerRigidBody.velocity = parentRigidBody.velocity;
+            gameObject.transform.parent = platform.transform;
             // TODO: player can move left and right on grid of log
         }
     }
 
-    void OnTriggerExit2D(Collider2D collision)
+    void OnTriggerExit2D(Collider2D platform)
     {
-        if (collision.gameObject.tag == "Player")
+        if (platform.gameObject.tag == "Rideable" && gameObject.transform.parent != null)
         {
             Debug.Log("Player left ride-able object");
-            collision.gameObject.transform.parent = null;
+            gameObject.transform.parent = null;
+            var playerRigidBody = GetComponent<Rigidbody2D>();
+            playerRigidBody.velocity = Vector3.zero;
             // TODO: Remove parent from Player gameobject
         }
     }
