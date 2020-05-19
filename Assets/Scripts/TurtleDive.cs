af6a9e6198e7a5dcc870fd45a841a008d7b52ce8
mccaua79 <drewmccauley90@gmail.com>
2020-05-19 18:55:20 -0500

    Adding turtle dive script

---
+++ Assets/Scripts/TurtleDive.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public enum DiveState
+{
+    AboveWater,
+    DivingUnder,
+    Underwater,
+    RisingUp
+}
+
+public class TurtleDive : MonoBehaviour
+{
+    public float TimeBetweenDives = 3f;
+    public float GracePeriodBetweenDivingFrames = 0.5f;
+    private float _timeSinceLastDive = 0;
+    private DiveState DiveState = DiveState.AboveWater;
+}
