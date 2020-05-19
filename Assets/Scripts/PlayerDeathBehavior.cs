94e0dbb97887198db0d3a3c09ed25e0b168b4de2
Andrew McCauley <>
2020-05-18 22:24:47 -0500

    Adding death sprite, fixing checks for squish, and drown

---
+++ Assets/Scripts/PlayerDeathBehavior.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class PlayerDeathBehavior : MonoBehaviour
+{
+    public Sprite PlayerNormal;
+    public Sprite PlayerDeath;
+
+    public void SetPlayerDeath()
+    {
+        Debug.Log("SetPlayerDeath");
+        var spriteRenderer = GetComponent<SpriteRenderer>();
+        spriteRenderer.sprite = PlayerDeath;
+    }
+
+    public void SetPlayerAlive()
+    {
+        Debug.Log("SetPlayerAlive");
+        var spriteRenderer = GetComponent<SpriteRenderer>();
+        spriteRenderer.sprite = PlayerNormal;
+    }
+}
