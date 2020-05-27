b33119fa6c22ee7cefaf9e7e997436961322efc4
Andrew McCauley <>
2020-05-26 22:29:59 -0500

    Working on the turtle animations, and diving

--- Assets/Scripts/TurtleDive.cs
+++ Assets/Scripts/TurtleDive.cs
@@ -1,17 +1,44 @@
 ﻿using UnityEngine;
 
-public enum DiveState
-{
-    AboveWater,
-    DivingUnder,
-    Underwater,
-    RisingUp
-}
-
 public class TurtleDive : MonoBehaviour
 {
     public float TimeBetweenDives = 3f;
     public float GracePeriodBetweenDivingFrames = 0.5f;
     private float _timeSinceLastDive = 0;
-    private DiveState DiveState = DiveState.AboveWater;
+    private bool _isUnderwater = false;
+
+    /// <summary>
+    /// Start the turtles diving under water
+    /// </summary>
+    /// <param name="isDiving"></param>
+    private void ToggleDive(bool isDiving)
+    {
+        foreach (Transform child in transform)
+        {
+            var obj = child.gameObject;
+            var animator = obj.GetComponent<Animator>();
+            animator.SetBool("isDiving", isDiving);
+        }
+
+        // Start timer
+        if (isDiving)
+        {
+            _timeSinceLastDive = TimeBetweenDives;
+        }
+
+        _isUnderwater = isDiving;
+    }
+
+    private void Update()
+    {
+        if (_isUnderwater)
+        {
+            _timeSinceLastDive -= Time.deltaTime;
+
+            if (_timeSinceLastDive <= 0)
+            {
+                ToggleDive(false);
+            }
+        }
+    }
 }
---
+++ Assets/Scripts/TurtleUnderwaterBehavior.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using UnityEngine;
+
+public class TurtleUnderwaterBehavior : MonoBehaviour
+{
+
+    /// <summary>
+    /// When the turtle reaches a point that the player can't stay on, remove collision
+    /// </summary>
+    /// <param name="value"></param>
+    public void ToggleCollision(int value)
+    {
+        bool isUnderwater = Convert.ToBoolean(value);
+        var obj = transform.parent.gameObject;
+        var collider = obj.GetComponent<BoxCollider2D>();
+        collider.enabled = !isUnderwater;
+    }
+}
